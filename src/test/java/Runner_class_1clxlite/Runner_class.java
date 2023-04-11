4edc9beba27e66f28bdc51f762d8d63a5b3e9669
Dineshkrishnan005 <dinesh.krishnan@roundrockabaschool.onmicrosoft.com>
2023-04-11 17:06:05 +0530

    Glue updated...

--- src/test/java/Runner_class_1clxlite/Runner_class.java
+++ src/test/java/Runner_class_1clxlite/Runner_class.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 
 @CucumberOptions(features = "C:\\Users\\Dinesh K\\eclipse-workspace\\1clxlite_version-1\\src\\test\\resources\\Features",
-glue= "Maret_Step_Definition" , 
+glue= "StepDefinition_1clxlite" , 
 plugin={"pretty","json:Report/1clxlite.json"
 		  ,"html:CucumberReport/1clxlite"
 		  ,"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
