6bd46190229a676a440ab54e593f64599853b240
Dineshkrishnan005 <dinesh.krishnan@roundrockabaschool.onmicrosoft.com>
2023-04-11 15:16:58 +0530

    Project started...

---
+++ src/test/java/Runner_class_1clxlite/Runner_class.java
@@ -0,0 +1,23 @@
+package Runner_class_1clxlite;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(features = "C:\\Users\\Dinesh K\\eclipse-workspace\\1clxlite_version-1\\src\\test\\resources\\Features",
+glue= "Maret_Step_Definition" , 
+plugin={"pretty","json:Report/1clxlite.json"
+		  ,"html:CucumberReport/1clxlite"
+		  ,"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+publish = true, 
+monochrome= true,
+dryRun= false)
+
+public class Runner_class {
+	
+	//C:\Users\Dinesh K\eclipse-workspace\1clxlite_version-1\src\test\resources\Features
+
+}
---
+++ src/test/java/StepDefinition_1clxlite/StepDefinition.java
@@ -0,0 +1,5 @@
+package StepDefinition_1clxlite;
+
+public class StepDefinition {
+
+}
